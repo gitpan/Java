6de7db0ab3fc068fc032a33db2ae96586a335c2d
Mark Ethan Trostler <mark@zzo.com>
2001-03-30 16:31:08 +0000

    Import of METZZO/Java-3.0 from CPAN.
    gitpan-cpan-distribution: Java
    gitpan-cpan-version:      3.0
    gitpan-cpan-path:         METZZO/Java-3.0.tar.gz
    gitpan-cpan-author:       METZZO
    gitpan-cpan-maturity:     released

--- examples/static.pl
+++ examples/static.pl
@@ -6,5 +6,13 @@ use Java;
 
 my $java = new Java();
 
-my $str = $java->get_field("java.util.Locale","CHINESE")->getDisplayName->get_value;
+my $field = $java->get_field("java.util.Locale","CHINESE");
+print STDERR "FIELD: $field\n";
+my $dispname = $field->getDisplayName;
+print STDERR "dispname: $field\n";
+my $str = $dispname->get_value;
 print "Chinese Locale: $str\n";
+
+$str = $java->create_object("java.lang.String","multie\nline\nstring\nhell");
+my $val = $str->get_value;
+print "VAL -$val-\n";
